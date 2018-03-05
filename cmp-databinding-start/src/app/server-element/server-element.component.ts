858c70effeb4f17859f3fbd8c11c628dd0d0f0b0
Felipe Hassad Pedrosa Rafful <felipe.rafful@programmers.com.br>
2018-03-05 16:59:44 -0300

    Seção 3

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input, ViewEncapsulation, ViewChild, ElementRef, ContentChild } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+})
+export class ServerElementComponent implements OnInit {
+  @Input('srvElement') element: { type: string, name: string, content: string };
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('Text Content: ', this.header.nativeElement.textContent);
+  }
+
+}
