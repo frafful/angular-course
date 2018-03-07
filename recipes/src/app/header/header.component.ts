0976f793af5d0089b7c8268d790be390acee7fb6
Felipe Hassad P Rafful <felipe.rafful@gmail.com>
2018-03-06 22:43:32 -0300

    Course Project

--- recipes/src/app/header/header.component.ts
+++ recipes/src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() menuSelected = new EventEmitter<string>();
 
+  onSelect(selectedMenu) {
+    this.menuSelected.emit(selectedMenu);
+  }
 }
--- recipes/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipes/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.module';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,13 +8,24 @@ import { Recipe } from '../recipe.module';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a test', 'https://images.unsplash.com/photo-1505334545366-6362e0c27a9e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b80d280b041eae0c88d97fbd851f44fc&auto=format&fit=crop&w=334&q=80')
+    new Recipe(
+                'A Test Recipe', 
+                'This is a test',
+                'https://images.unsplash.com/photo-1505334545366-6362e0c27a9e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b80d280b041eae0c88d97fbd851f44fc&auto=format&fit=crop&w=334&q=80',
+                [new Ingredient('Apples', 7), new Ingredient('Oranges', 3)]
+              )
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
