ea54851a832418db9e88feae092f4f79934c91c7
Felipe Hassad P Rafful <felipe.rafful@gmail.com>
2018-03-07 21:36:14 -0300

    Session 6 - Recipe app improvements

--- recipes/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ recipes/src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
+  @Output() onIngredientAdded = new EventEmitter<{ name, amount }>();
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddIngredientClick() {
+    this.onIngredientAdded.emit({ name: this.nameInputRef.nativeElement.value, amount: this.amountInputRef.nativeElement.value });
   }
 
 }
